6a1b68d6235d718a94cee922714c0b612e72b514
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-07-12 23:06:43 +0200

    Lesson 2 Rev 1 - TodoList component w/ todoList Array.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,11 @@
-function TodoList () {
+function TodoList() {
+  const todoList = [
+    { id: 1, title: "review resources" },
+    { id: 2, title: "take notes" },
+    { id: 3, title: "code out app" },
+  ];
     return (
-        <></>
+      <></>
     );
 }
 
