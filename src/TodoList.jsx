32c66a72814c421b25ef2b5e6b16199b3b994315
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-07-20 10:41:25 +0200

    Lesson 3 - Implement useState, Update TodoList Props, Refactor TodoList.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,9 @@
-function TodoList() {
-  const todoList = [
-    { id: 1, title: 'review resources' },
-    { id: 2, title: 'take notes' },
-    { id: 3, title: 'code out app' },
-  ];
+function TodoList({todoList}) {
+  // const todoList = [
+  //   { id: 1, title: 'review resources' },
+  //   { id: 2, title: 'take notes' },
+  //   { id: 3, title: 'code out app' },
+  // ];
     return (
       <ul>
         {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
