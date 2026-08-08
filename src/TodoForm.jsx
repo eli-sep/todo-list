26e559295e755bccc673fbcaa979f6bfab85a1e6
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-08-07 23:55:59 -0700

    Lesson 5 Revised - Removed 'if(workingTodoTitle.trim())' guard

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,31 +1,23 @@
 import { useState, useRef } from 'react';
 
 function TodoForm({ onAddTodo }) {
-  const [workingTodoTitle, setWorkingTodoTitle] = useState('')
+  const [workingTodoTitle, setWorkingTodoTitle] = useState('');
   const inputRef = useRef();
 
   function handleAddTodo(event) {
     event.preventDefault();
 
-    if (workingTodoTitle.trim()) {
     onAddTodo(workingTodoTitle);
     inputRef.current.focus();
-    setWorkingTodoTitle('')
+    setWorkingTodoTitle('');
   }
-}
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input 
-      ref={inputRef} 
-      type="text" 
-      id="todoTitle" 
-      name="todoTitle" 
-      placeholder={'Todo text'} 
-      required
-      value={workingTodoTitle}
-      onChange={event => setWorkingTodoTitle(event.target.value)} />
-      <button type="submit" disabled={!workingTodoTitle.trim()}>Add Todo</button>
+      <input ref={inputRef} type="text" id="todoTitle" name="todoTitle" placeholder={'Todo text'} required value={workingTodoTitle} onChange={(event) => setWorkingTodoTitle(event.target.value)} />
+      <button type="submit" disabled={!workingTodoTitle.trim()}>
+        Add Todo
+      </button>
     </form>
   );
 }
