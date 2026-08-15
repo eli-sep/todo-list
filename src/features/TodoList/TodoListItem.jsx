d00d1bdc27f1b11e3dd3498c2ab4fa71a33f465f
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-08-14 21:56:27 -0700

    Lesson 6 - Re-organize Project

---
+++ src/features/TodoList/TodoListItem.jsx
@@ -0,0 +1,10 @@
+function TodoListItem({ todo, onCompleteTodo }) {
+  return (
+    <li>
+      <input type="checkbox" checked={todo.isCompleted} onChange={() => onCompleteTodo(todo.id)} />
+      {todo.title}
+    </li>
+  );
+}
+
+export default TodoListItem;
