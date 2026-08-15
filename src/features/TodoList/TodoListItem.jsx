f9a4871cbaa1c2a4dabcf61f20b5c0626228e690
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-08-15 10:27:29 -0700

    Lesson 6 - Continue Building Edit Functionality

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -3,14 +3,20 @@ import TextInputWithLabel from '../../shared/TextInputWithLabel';
 
 function TodoListItem({ todo, onCompleteTodo }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [workingTodoTitle, setWorkingTodoTitle] = useState(todo.title)
+
+  function handleCancel(){
+    setWorkingTodoTitle(todo.title);
+    setIsEditing(false);
+  }
 
   return (
     <li>
       <form>
-        {isEditing ? (
+        {isEditing ? (<>
           <TextInputWithLabel value={todo.title} />
-        ) : (
-          /* <button onClick={() => setIsEditing(false)}>Save</button> */
+          <button type='button' onClick={() => handleCancel()}>Cancel</button>
+        </>) : (
           <>
             <label>
               <input 
